223c0cb83c2ea74dc07486c42e7c9da615700a3b
amir <amirben1991@gmail.com>
2024-10-13 22:14:14 +0200

    new component + update page-head component --> add welcome message and interactive button

--- src/app/page-head/page-head.component.ts
+++ src/app/page-head/page-head.component.ts
@@ -9,17 +9,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-head.component.html',
   styleUrl: './page-head.component.scss'
 })
+
+
 export class PageHeadComponent implements OnInit{
   title!: string;
   description!: string;
   date!: Date;
   imageUrl!: string;
+  userInput!: string;
 
   ngOnInit() : void {
-    this.title = "Welcome to my personnal website";
+    this.title = "Welcome to my personnal website: ";
     this.description = "This is where you can visit my profile, and have an extended view about my skills";
     this.date = new Date();
     this.imageUrl = "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg";
   }
 
+  getUserInput() {
+    const input = prompt("Please enter your name");
+    if (input !== null && input.trim() !== '') {
+      this.userInput = `Welcome, ${input}!`;
+    }
+  }
 }
---
+++ src/app/principal-content/principal-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal-content.component.html',
+  styleUrl: './principal-content.component.scss'
+})
+export class PrincipalContentComponent {
+
+}
