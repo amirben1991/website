f28ca0f01ac33fda2400fe65248e8c8d48198327
amir <amirben1991@gmail.com>
2024-10-13 21:39:07 +0200

    add image + add some text

--- src/app/page-head/page-head.component.ts
+++ src/app/page-head/page-head.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-head',
@@ -9,6 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './page-head.component.html',
   styleUrl: './page-head.component.scss'
 })
-export class PageHeadComponent {
+export class PageHeadComponent implements OnInit{
+  title!: string;
+  description!: string;
+  date!: Date;
+  imageUrl!: string;
+
+  ngOnInit() : void {
+    this.title = "Welcome to my personnal website";
+    this.description = "This is where you can visit my profile, and have an extended view about my skills";
+    this.date = new Date();
+    this.imageUrl = "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg";
+  }
 
 }
