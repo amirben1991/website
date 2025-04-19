f5f2fac8aecb4e91e2eb61708ef64461b8675151
amirben1991 <amirben1991@gmail.com>
2025-04-20 00:20:08 +0200

    test insert nav menu in education component

--- src/app/principal-content/education-content/education-content.component.ts
+++ src/app/principal-content/education-content/education-content.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
     selector: 'app-education-content',
-    imports: [],
+    imports: [MatMenuModule, MatButtonModule],
     templateUrl: './education-content.component.html',
     styleUrl: './education-content.component.scss'
 })
