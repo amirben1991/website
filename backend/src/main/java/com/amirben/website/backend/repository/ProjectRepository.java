1aa467d9898f80c5eee1edf2347ca9eedc5929f0
Amir <amirben1991@gmail.com>
2026-01-19 21:31:14 +0100

    Creation Repositories

---
+++ backend/src/main/java/com/amirben/website/backend/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.amirben.website.backend.repository;
+
+import com.amirben.website.backend.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+}
